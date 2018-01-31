c9e245dc32a7426581cafd328026bb8356daf2a7
Aniket Pradhan <aniket17133@iiitd.ac.in>
2018-01-31 20:56:30 +0530

    Added disco mode in app and in final.ino

--- Final/Final.ino/Final.ino.ino
+++ Final/Final.ino/Final.ino.ino
@@ -1,8 +1,8 @@
 //PROGRAM MADE BY:
-//  -ANIKET PRADHAN
-//  -AAYUSH GUPTA
-//  -ARAV MALIK
-//  -SAI SANTOSH
+//	-ANIKET PRADHAN
+//	-AAYUSH GUPTA
+//	-ARAV MALIK
+//	-SAI SANTOSH
 //
 //THE PROGRAM IS MADE FOR THE IED MINI PROJECT, I.E. THE SMART LAMP.
 //IT USES VARIOUS SENSORS AND MOTORS FOR ITS WORKING.
@@ -12,12 +12,18 @@
 #include <Servo.h>
 
 //PIN INFO
-//LED -> 3
-//LDR -> A0
-//SERVO -> 9
+//LED -> 3 PWM
+//LDR -> A0 Analog Input
+//SERVO -> 9 PWM
+//RED -> 5 PWM
+//BLUE -> 11 PWM
+//GREEN -> 10 PWM
 
 //GLOBAL VARIABLES
 int ledpin = 3;
+int redPin = 5; //IT HAS TO BE A PWM
+int greenPin = 10;
+int bluePin = 11;
 int ldrpin = A0;
 int value = 0;
 Servo myservo;
@@ -31,63 +37,102 @@ int echoPin = 4;
 
 //SETUP CODE
 void setup() {
-  myservo.attach(9);
-  Serial.begin(9600);
-  pinMode(ledpin,OUTPUT);
-  pinMode(echoPin,INPUT);    //Echo
-  pinMode(trigPin,OUTPUT);    //Trigger
-  digitalWrite(ledpin,LOW);
-  digitalWrite(trigPin,LOW);
-//  pinMode(ledPin, OUTPUT);
-//  bluetooth.begin(9600);
+ 	myservo.attach(9);
+	Serial.begin(9600);
+	pinMode(ledpin,OUTPUT);
+	pinMode(echoPin,INPUT);    //Echo
+	pinMode(trigPin,OUTPUT);    //Trigger
+	digitalWrite(ledpin,LOW);
+	digitalWrite(trigPin,LOW);
+	pinMode(redPin, OUTPUT);
+	pinMode(greenPin, OUTPUT);
+	pinMode(bluePin, OUTPUT);
+//	pinMode(ledPin, OUTPUT);
+//	bluetooth.begin(9600);
 }
 
 //LOOPING CODE
 void loop() {
-  if(Serial.available() > 0){
-    int toSend = Serial.read();
-    if(toSend == 190){
-      //Move dc motor UP
-    }
-    if(toSend == 191){
-      //Move dc motor down
-    }
-    if(toSend == 192){
-      //Move dc motor to default location
-    }
-    Serial.print(toSend);
-    Serial.print("\n");
-    myservo.write(toSend);
-  }
-  
-  //Add code for LDR
-  value = analogRead(ldrpin);
-  value = constrain(value,900,1010);
-  value = map(value,900,1010,255,0);
-  Serial.println(value);
-  analogWrite(ledpin, value);
-//  delay(30);
-  
-  //Add code for Switch
-  int time2,tim;
-  digitalWrite(trigPin,HIGH);
-  delayMicroseconds(100);
-  digitalWrite(trigPin,LOW); 
-  time2 = pulseIn(echoPin,HIGH);
-  tim = time2/2;
-  int dist = 0.034*tim;
-  Serial.println(dist);
-  if (dist<20 && dist>0){
-    if (t==1){
-      digitalWrite(ledpin,HIGH);
-      t=0;
-    }
-    else{
-      digitalWrite(ledpin,LOW);
-      t=1;
-    }
-  }
-//  delay(150);
+	if(Serial.available() > 0){
+		int toSend = Serial.read();
+		if(toSend == 200) {
+			//Add code for varying RGB lights
+			setColor(1, 0, 0);  // red lowest brightness
+			delay(1000);
+			setColor(50, 0, 0);  // red
+			delay(1000);
+			setColor(140, 0, 0);  // red
+			delay(1000);
+			setColor(255, 0, 0);  // red
+			delay(1000);
+			setColor(0, 255, 0);  // green
+			delay(1000);
+			setColor(0, 0, 255);  // blue
+			delay(1000);
+			setColor(255, 255, 0);  // yellow
+			delay(1000);  
+			setColor(80, 0, 80);  // purple
+			delay(1000);
+			setColor(255, 50, 0);  // Orange
+			delay(1000);
+		}
+		else {
+			//Add code for LDR
+			value = analogRead(ldrpin);
+			value = constrain(value,900,1010);
+			value = map(value,900,1010,255,0);
+			Serial.println(value);
+			analogWrite(ledpin, value);
+			//delay(30);
+		
+			//Add code for Switch
+			int time2,tim;
+			digitalWrite(trigPin,HIGH);
+			delayMicroseconds(100);
+			digitalWrite(trigPin,LOW); 
+			time2 = pulseIn(echoPin,HIGH);
+			tim = time2/2;
+			int dist = 0.034*tim;
+			Serial.println(dist);
+			if (dist<20 && dist>0){
+				if (t==1){
+				digitalWrite(ledpin,HIGH);
+				t=0;
+				}
+			else{
+				digitalWrite(ledpin,LOW);
+				t=1;
+				}
+			}
+
+			//Writes to DC Motor
+			if(toSend == 190){
+				//Move dc motor UP, rotate motor clockwise.
+			}
+			if(toSend == 191){
+				//Move dc motor down, rotate motor anticlockwise
+			}
+			if(toSend == 192){
+				//Move dc motor to default location
+			}
+
+			//Writes to servo
+			//Serial.print(toSend);
+			//Serial.print("\n");
+			myservo.write(toSend);
+				}
+			}
+			delay(150);
+
+	
+}
+
+//Extra Functions
+void setColor(int red, int green, int blue)
+{
+  analogWrite(redPin, red);
+  analogWrite(greenPin, green);
+  analogWrite(bluePin, blue);  
 }
 
 //TO-DO
---
+++ RGB/RGBexample1.ino
@@ -0,0 +1,31 @@
+int redPin = 9;
+int greenPin = 10;
+int bluePin = 11;
+
+ 
+void setup()
+{
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);  
+}
+ 
+void loop()
+{
+  digitalWrite(redPin,HIGH);
+  delay(1000);
+  digitalWrite(redPin,LOW);
+  digitalWrite(greenPin,HIGH);
+  digitalWrite(bluePin,LOW);
+  delay(1000);
+  digitalWrite(redPin,LOW);
+  digitalWrite(greenPin,LOW);
+  digitalWrite(bluePin,HIGH);
+  delay(1000);
+  digitalWrite(bluePin,LOW);
+  digitalWrite(greenPin,LOW);
+  digitalWrite(redPin,LOW);
+  delay(1000);
+}
+ 
+
